a6990b164d2834b1ba1f256990bb580c80f02b67
Afatyu <a.fatahyudha20@gmail.com>
2020-11-04 15:53:26 +0700

    Program Pempraktekan

---
+++ Latihan1.py
@@ -0,0 +1,6 @@
+for a in range(10): #angka a berjumlah 10 angka
+   for b in range(10): #angka b berjumlah 10 angka
+      x = a+b #rumus dimana angka a+b, agar setiap pengulangan baris baru dimulai dengan angka +1
+      print(x, end=' ') #print 10 angka memanjang dan memberi spasi setiap print angka baru
+   print() #print ulang setelah 10 angka
+
